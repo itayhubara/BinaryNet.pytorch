3950a4b3a561aaaae987e8c69d8720c8cdc6839a
Gauthier-Clerc François <francois.gauthier-clerc@insa-rennes.fr>
2019-03-26 11:21:24 -0600

    fix error on Hinge label and model output dim.

--- models/alexnet.py
+++ models/alexnet.py
@@ -74,5 +74,5 @@ class AlexNetOWT_BN(nn.Module):
 
 
 def alexnet(**kwargs):
-    num_classes = getattr(kwargs, 'num_classes', 1000)
+    num_classes = kwargs.get( 'num_classes', 1000)
     return AlexNetOWT_BN(num_classes)
